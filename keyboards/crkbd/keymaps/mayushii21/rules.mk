b20efae1f43313e35cf5500384c26786b2abbab4
mayushii21 <122178787+mayushii21@users.noreply.github.com>
2024-10-31 23:32:08 -0500

    commit config and rules

---
+++ keyboards/crkbd/keymaps/mayushii21/rules.mk
@@ -0,0 +1,11 @@
+OLED_ENABLE = no
+TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = no
+CAPS_WORD_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+# REPEAT_KEY_ENABLE = yes
+DYNAMIC_TAPPING_TERM_ENABLE = yes
+
+SRC += features/achordion.c
+SRC += features/layer_lock.c
+
